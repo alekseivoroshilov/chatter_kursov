142ce4b3df0d683b1776174813d7bf692ab1d861
alekseivoroshilov <nekobeardetected@gmail.com>
2020-01-27 02:40:25 +0300

    tests 2

--- app/src/androidTest/java/com/example/chat/LoginTest.java
+++ app/src/androidTest/java/com/example/chat/LoginTest.java
@@ -1,15 +1,22 @@
 package com.example.chat;
 
+import android.view.View;
+
 import androidx.test.espresso.Espresso;
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.espresso.matcher.ViewMatchers;
 
+import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 public class LoginTest {
@@ -29,13 +36,29 @@ public class LoginTest {
         Espresso.onView(withId(R.id.register)).perform(ViewActions.click());
         intended(hasComponent(Registration.class.getName()));
         Espresso.onView(withId(R.id.login)).perform(ViewActions.click());
-        //intended(hasComponent(Login.class.getName()));
     }
-    /*@Test
+    @Test
     public void testEnter() {
         Espresso.onView(withId(R.id.username)).perform(ViewActions.typeText("testt"));
         Espresso.onView(withId(R.id.password)).perform(ViewActions.typeText("testt"));
         Espresso.onView(withId(R.id.loginButton)).perform(ViewActions.click());
+        Espresso.onView(isRoot()).perform(waitFor(5000));
         intended(hasComponent(MainActivity.class.getName()));
-    }*/
+        pressBack();
+    }
+    public static ViewAction waitFor(long delay) {
+        return new ViewAction() {
+            @Override public Matcher<View> getConstraints() {
+                return isRoot();
+            }
+
+            @Override public String getDescription() {
+                return "wait for " + delay + "milliseconds";
+            }
+
+            @Override public void perform(UiController uiController, View view) {
+                uiController.loopMainThreadForAtLeast(delay);
+            }
+        };
+    }
 }
